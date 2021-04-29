c8c6d1b86cd99afd21dced0d042e3f4f7c1eb6a2
SOCINFORM\Danil.Krylov <danil.krylov@onlineinterviewer.ru>
2021-04-29 12:58:10 +0300

    'add-context-provider'

--- src/components/MusicPlayer/Content.js
+++ src/components/MusicPlayer/Content.js
@@ -1,11 +1,13 @@
 /** @jsx jsx */
-import React from 'react'
+import React, { useContext } from 'react'
+import { StoreContext } from './index'
 import { css, jsx } from '@emotion/react'
 
-const Content = ({ children }) => {
+const Content = () => {
+  const { state } = useContext(StoreContext)
   return (
     <div className="Content" css={CSS}>
-      {children}
+      {state.currentPlaylist}
     </div>
   )
 }
@@ -14,6 +16,9 @@ const CSS = css`
   width: calc(100% - 200px);
   padding: 20px;
   background: #121212;
+  padding-top: 80px;
+  font-size: 20px;
+  text-transform: capitalize;
 `
 
 export default Content
--- src/components/MusicPlayer/index.js
+++ src/components/MusicPlayer/index.js
@@ -1,19 +1,47 @@
 /** @jsx jsx */
-import React from 'react'
+import React, { useReducer, createContext } from 'react'
 import { css, jsx } from '@emotion/react'
 import Topbar from './Topbar'
 import Sidebar from './Sidebar'
 import Playbar from './Playbar'
 import Content from './Content'
 
+export const StoreContext = createContext(null)
+
+const DEFAULT_PLAYLIST = 'home'
+
+const initialState = {
+  currentPlaylist: DEFAULT_PLAYLIST,
+  playlists: {
+    home: new Set(),
+    favorites: new Set()
+  }
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_PLAYLIST':
+      return {
+        ...state,
+        playlists: { ...state.playlists, [action.playlist]: new Set() }
+      }
+    case 'SET_PLAYLIST':
+      return { ...state, currentPlaylist: action.playlist }
+  }
+  return state
+}
+
 const MusicPlayer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="MusicPlayer" css={CSS}>
-      <Topbar />
-      <Sidebar />
-      <Content />
-      <Playbar />
-    </div>
+    <StoreContext.Provider value={{ state, dispatch }}>
+      <div className="MusicPlayer" css={CSS}>
+        <Topbar />
+        <Sidebar />
+        <Content></Content>
+        <Playbar />
+      </div>
+    </StoreContext.Provider>
   )
 }
 
